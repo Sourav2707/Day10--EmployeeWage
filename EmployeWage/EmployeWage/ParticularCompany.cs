395b4c81a0ab0202e8c2187a91d02e4b9900751f
Sourav2707 <smartsourav33@gmail.com>
2022-07-16 22:31:53 +0530

    Employe wage builder for each company

---
+++ EmployeWage/EmployeWage/ParticularCompany.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWage
+{
+    public class ParticularCompany                          //stores the particular company details
+    {
+        public String company;
+        public int WaPhr;
+        public int max_workDays;
+        public int max_workHrs;
+        public int totalWage;
+        public ParticularCompany(String company, int WaPhr, int max_workDays, int max_workHrs)
+        {
+            this.company = company;
+            this.WaPhr = WaPhr;
+            this.max_workDays = max_workDays;
+            this.max_workHrs = max_workHrs;
+        }
+        public void TWage(int totalWage)                    //TWage stores the total wage of an employee in a company
+        {
+            this.totalWage = totalWage;
+            Console.WriteLine(this.totalWage);
+        }
+    }
+}
