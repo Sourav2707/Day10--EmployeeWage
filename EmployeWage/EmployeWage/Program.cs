1c1f12d1eb5afdc390a647091e85d299f46a3dec
Sourav2707 <smartsourav33@gmail.com>
2022-07-18 11:55:30 +0530

    storing dailywage with monthlywage

--- EmployeWage/EmployeWage/Program.cs
+++ EmployeWage/EmployeWage/Program.cs
@@ -1,10 +1,9 @@
-﻿//Already using class since starting
-using EmployeeWage;
-Console.WriteLine("Welcome to employee wage computation program");
+﻿using EmployeeWage;
+Console.WriteLine("Welcome to Employee Wage Computation program");
 IEmployeeWageForCompany comp = new Wages();
-comp.AddDetails("TATA", 200, 20, 8);
-comp.AddDetails("IBM", 300, 25, 9);
-comp.AddDetails("TCS", 200, 25, 9);
-comp.AddDetails("Mahindra", 280, 20, 8);
-comp.AddDetails("L&T", 150, 27, 9);
+comp.AddDetails("TATA", 200, 20, 100);
+comp.AddDetails("IBM", 300, 25, 100);
+comp.AddDetails("TCS", 200, 25, 100);
+comp.AddDetails("Mahindra", 280, 20, 100);
+comp.AddDetails("L&T", 150, 27, 100);
 comp.perCompany();
